8a8a23cc7c18e084324b94bc16654300360390f5
abe <aborg@csumb.edu>
2023-12-17 13:59:59 -0800

    est q vals for parts 41 - 50 complete.

--- src/main.py
+++ src/main.py
@@ -11,8 +11,8 @@ import Bradley
 if __name__ == '__main__':
 
     # ========== GENERATE Q ESTIMATES ==========
-    chess_data_file_path = game_settings.chess_games_filepath_part_40
-    est_q_vals_file_path = game_settings.est_q_vals_filepath_part_40
+    chess_data_file_path = game_settings.chess_games_filepath_part_50
+    est_q_vals_file_path = game_settings.est_q_vals_filepath_part_50
 
     chess_data = pd.read_pickle(chess_data_file_path, compression = 'zip')
     chess_data = chess_data.head(game_settings.training_sample_size)
