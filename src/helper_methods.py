3a37afd6df05046dca0ccf9562315b7acf866e90
Abey <abrahamborg@gmail.com>
2024-07-13 08:11:20 -0700

    mov play_turn outside agent_vs_agent helper method

--- src/helper_methods.py
+++ src/helper_methods.py
@@ -9,7 +9,7 @@ def play_game(bubs, chess_agent) -> None:
     while bubs.is_game_over() == False:
         try:
             print(f'\nCurrent turn is :  {bubs.environ.get_curr_turn()}\n')
-            chess_move = handle_move(player_turn)
+            chess_move = handle_move(player_turn, chess_agent)
             print(f'{player_turn} played {chess_move}\n')
         except Exception as e:
             # put in logger here.
@@ -22,7 +22,7 @@ def play_game(bubs, chess_agent) -> None:
     bubs.reset_environ()
 ### end of play_game
 
-def handle_move(player_color):
+def handle_move(player_color, chess_agent) -> str:
     if player_color == chess_agent.color:
         print('=== RL AGENT\'S TURN ===\n')
         return bubs.rl_agent_selects_chess_move(player_color, chess_agent)
@@ -35,21 +35,14 @@ def handle_move(player_color):
         return move
 
 def agent_vs_agent(bubs, w_agent, b_agent) -> None:
-    def play_turn(chess_agent):
-        try:
-            chess_move = bubs.rl_agent_selects_chess_move(chess_agent.color)
-            # agent_vs_agent_file.write(f'{chess_agent.color} agent played {chess_move}\n')
-        except Exception as e:
-            # agent_vs_agent_file.write(f'An error occurred: {e}\n')
-            raise Exception from e
-
     try:
         while bubs.is_game_over() == False:
             # agent_vs_agent_file.write(f'\nCurrent turn: {bubs.environ.get_curr_turn()}')
-            play_turn('W')
+            play_turn(w_agent)
             
+            # sometimes game ends after white's turn
             if bubs.is_game_over() == False:
-                play_turn('B')
+                play_turn(b_agent)
 
         # agent_vs_agent_file.write('Game is over, chessboard looks like this:\n')
         # agent_vs_agent_file.write(bubs.environ.board + '\n\n')
@@ -61,6 +54,14 @@ def agent_vs_agent(bubs, w_agent, b_agent) -> None:
 
     bubs.reset_environ()
 ### end of agent_vs_agent
+
+def play_turn(chess_agent):
+    try:
+        chess_move = bubs.rl_agent_selects_chess_move(chess_agent)
+        # agent_vs_agent_file.write(f'{chess_agent.color} agent played {chess_move}\n')
+    except Exception as e:
+        # agent_vs_agent_file.write(f'An error occurred: {e}\n')
+        raise Exception from e
 
 def pikl_q_table(bubs, chess_agent, q_table_path: str) -> None:
     chess_agent.q_table.to_pickle(q_table_path, compression = 'zip')
