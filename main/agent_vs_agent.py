7c992dc869019209d0b78ddfec09aceeccefb128
Abey <abrahamborg@gmail.com>
2024-11-01 12:15:50 -0700

    removed print statements

--- main/agent_vs_agent.py
+++ main/agent_vs_agent.py
@@ -5,7 +5,7 @@ from agents import Agent
 from utils.logging_config import setup_logger 
 agent_vs_agent_logger = setup_logger(__name__, game_settings.agent_vs_agent_logger_filepath)
 
-def agent_vs_agent(environ, w_agent, b_agent, print_to_screen = False, current_game = 0) -> None:
+def agent_vs_agent(environ, w_agent, b_agent, print_to_screen = False, current_game: int = 0) -> None:
     agent_vs_agent_logger.info(f'Playing game {current_game}\n')
     try:    
         while helper_methods.is_game_over(environ) == False:
@@ -14,7 +14,6 @@ def agent_vs_agent(environ, w_agent, b_agent, print_to_screen = False, current_g
             agent_vs_agent_logger.info(f'White agent played {chess_move}')
             agent_vs_agent_logger.info(f'Current turn is: {environ.get_current_turn()}. \nWhite agent played {chess_move}\n')
      
-            # sometimes game ends after white's turn
             if helper_methods.is_game_over(environ) == False:
                 chess_move = helper_methods.agent_selects_and_plays_chess_move(b_agent, environ)
                 agent_vs_agent_logger.info(f'Black agent played {chess_move} curr board is:\n{environ.board}\n')
@@ -38,12 +37,8 @@ if __name__ == '__main__':
         bradley = helper_methods.bootstrap_agent(bradley, game_settings.bradley_agent_q_table_path)
         imman = helper_methods.bootstrap_agent(imman, game_settings.imman_agent_q_table_path)
         number_of_games = int(input('How many games do you want the agents to play? '))
-        print_to_screen = (input('Do you want to print the games to the screen? (y/n) ')).lower()[0]
-
+        
         for current_game in range(int(number_of_games)):
-            if print_to_screen == 'y':
-                print(f'Game {current_game + 1}')
-
             agent_vs_agent(environ, bradley, imman, print_to_screen, current_game)
     except Exception as e:
         print(f'agent vs agent interrupted because of:  {e}')
@@ -52,7 +47,6 @@ if __name__ == '__main__':
     
     end_time = time.time()
     total_time = end_time - start_time
-
     print('single agent vs agent game is complete')
     print(f'it took: {total_time}')
 
