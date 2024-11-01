d6220ccc74b723204a2e365c308d60cc125ad90a
Abey <abrahamborg@gmail.com>
2024-11-01 13:40:48 -0700

    simplified docstrings

--- utils/helper_methods.py
+++ utils/helper_methods.py
@@ -7,20 +7,10 @@ helper_methods_logger = setup_logger(__name__, game_settings.helper_methods_erro
 
 def agent_selects_and_plays_chess_move(chess_agent, environ) -> str:
     """
-        The Agent selects a chess move and loads it onto the chessboard.
-        This method allows the agent to select a chess move and load it onto the 
-        chessboard. It is used during actual gameplay between the computer and the user, not during training. The 
-        method first gets the current state of the environment. If the list of legal moves is empty, an exception 
-        is raised. Depending on the color of the RL agent, the appropriate agent selects a move. The selected move 
-        is then loaded onto the chessboard and the current state of the environment is updated.
-        Args:
+        The Agent selects a chess move and loads it onto the chessboard. It is used 
+        during actual gameplay between the computer and the user, not during training. 
         Returns:
             str: A string representing the selected chess move.
-        Raises:
-            StateUpdateError: If the current state is not valid or fails to update.
-            NoLegalMovesError: If the list of legal moves is empty.
-            ChessboardLoadError: If the chessboard fails to load the move.
-            StateRetrievalError: If the current state is not valid or fails to retrieve.
         Side Effects:
             Modifies the chessboard and the current state of the environment by loading the chess move and updating 
             the current state.
@@ -34,20 +24,14 @@ def agent_selects_and_plays_chess_move(chess_agent, environ) -> str:
 
 def receive_opponent_move(chess_move: str, environ) -> bool:                                                                                 
     """
-        Receives the opponent's chess move and updates the environment.
-        This method receives the opponent's chess move, loads it onto the chessboard, and updates the current state 
-        of the environment. If an error occurs while loading the chessboard or updating the current state, an error 
-        message is written to the errors file and an exception is raised.
-
+        receives the opponent's chess move, loads it onto the chessboard, and updates the current state 
+        of the environment.
         Args:
             chess_move (str): A string representing the opponent's chess move, such as 'Nf3'.
         Returns:
             bool: A boolean value indicating whether the move was successfully loaded and the current state was 
             successfully updated. Returns False if an error occurred while loading the chessboard, and does not 
             attempt to update the current state.
-        Raises:
-            Exception: An exception is raised if the chessboard fails to load the move or if the current state fails 
-            to update. The original exception is included in the raised exception.
         Side Effects:
             Modifies the chessboard and the current state of the environment by loading the chess move and updating 
             the current state.
@@ -55,7 +39,6 @@ def receive_opponent_move(chess_move: str, environ) -> bool:
     environ.load_chessboard(chess_move)
     environ.update_curr_state()
     return True
-
 ### end of receive_opp_move
 
 def pikl_q_table(chess_agent, q_table_path: str) -> None:
@@ -88,17 +71,11 @@ def reset_q_table(q_table) -> None:
 
 def is_game_over(environ) -> bool:
     """
-        This method determines whether the game is over based on three conditions: if the game is over according to 
-        the chessboard, if the current turn index has reached the maximum turn index defined in the game settings, 
-        or if there are no legal moves left.
-
+        determines whether the game is over
         Arg: environ object, which manages a chessboard
-
         Returns:
-            bool: A boolean value indicating whether the game is over. Returns True if any of the three conditions 
-            are met, and False otherwise.
-        Side Effects:
-            None.
+            bool: A boolean value indicating whether the game is over. 
+            Returns True if any of the three conditions are met.
     """
     try:
         return (
@@ -114,17 +91,10 @@ def is_game_over(environ) -> bool:
 
 def get_game_outcome(environ) -> str:
     """
-        Returns the outcome of the chess game.
-        This method returns the outcome of the chess game. It calls the `outcome` method on the chessboard, which 
-        returns an instance of the `chess.Outcome` class, and then calls the `result` method on this instance to 
-        get the outcome of the game. If an error occurs while getting the game outcome, an error message is 
-        returned.
-
+        returns the outcome of the chess game. It calls the `outcome` method on the chessboard.
         Returns:
             str: A string representing the outcome of the game. The outcome is a string in the format '1-0', '0-1', 
-            or '1/2-1/2', representing a win for white, a win for black, or a draw, respectively. If an error 
-            occurred while getting the game outcome, the returned string starts with 'error at get_game_outcome: ' 
-            and includes the error message.
+            or '1/2-1/2'
         Raises:
             GameOutcomeError: If the game outcome cannot be determined.
     """
@@ -138,20 +108,14 @@ def get_game_outcome(environ) -> str:
 
 def get_game_termination_reason(environ) -> str:
     """
-        Returns a string that describes the reason for the game ending.
-        This method returns a string that describes the reason for the game ending. It calls the `outcome` method 
-        on the chessboard, which returns an instance of the `chess.Outcome` class, and then gets the termination 
-        reason from this instance. If an error occurs while getting the termination reason, an error message is 
-        returned.
-
+        returns a string that describes the reason for the game ending. It calls the `outcome` method 
+        on the chessboard.
         Returns:
             str: A string representing the reason for the game ending. 
             If an error occurred while getting the termination reason, the returned string starts with 'error at 
             get_game_termination_reason: ' and includes the error message.
         Raises:
             GameTerminationError: If the termination reason cannot be determined.
-        Side Effects:
-            None.
     """
     try:
         return str(environ.board.outcome().termination)
