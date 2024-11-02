2a622aac33d1fe4f884eb27a31fab19b990fea02
Abey <abrahamborg@gmail.com>
2024-11-02 15:15:45 -0700

    removed logging from helper methods

--- utils/helper_methods.py
+++ utils/helper_methods.py
@@ -21,18 +21,6 @@ def agent_selects_and_plays_chess_move(chess_agent, environ) -> str:
 ### end of agent_selects_and_plays_chess_move
 
 def receive_opponent_move(chess_move: str, environ) -> bool:                                                                                 
-    """
-        receives the opponent's chess move, loads it onto the chessboard, and updates the current state 
-        of the environment.
-        Args:
-            chess_move (str): A string representing the opponent's chess move, such as 'Nf3'.
-        Returns:
-            bool: A boolean value indicating whether the move was successfully loaded and the current state was 
-            successfully updated. 
-        Side Effects:
-            Modifies the chessboard and the current state of the environment by loading the chess move and updating 
-            the current state.
-    """
     environ.load_chessboard(chess_move)
     environ.update_curr_state()
     return True
@@ -67,57 +55,17 @@ def reset_q_table(q_table) -> None:
 ### end of reset_q_table ###
 
 def is_game_over(environ) -> bool:
-    """
-        determines whether the game is over
-        Arg: environ object, which manages a chessboard
-        Returns:
-            bool: A boolean value indicating whether the game is over. 
-            Returns True if any of the three conditions are met.
-    """
-    try:
-        return (
-            environ.board.is_game_over() or
-            environ.turn_index >= constants.max_turn_index or
-            (len(environ.get_legal_moves()) == 0)
-        )
-    except Exception as e:
-        error_message = f'error at is_game_over: {str(e)}, failed to determine if game is over\n'
-        helper_methods_logger.error(error_message)
-        raise custom_exceptions.GameOverError(error_message) from e
+    return (
+        environ.board.is_game_over() or
+        environ.turn_index >= constants.max_turn_index or
+        (len(environ.get_legal_moves()) == 0)
+    )
 ### end of is_game_over
 
 def get_game_outcome(environ) -> str:
-    """
-        returns the outcome of the chess game. It calls the `outcome` method on the chessboard.
-        Returns:
-            str: A string representing the outcome of the game. The outcome is a string in the format '1-0', '0-1', 
-            or '1/2-1/2'
-        Raises:
-            GameOutcomeError: If the game outcome cannot be determined.
-    """
-    try:
-        return environ.board.outcome().result()
-    except Exception as e:
-        error_message = f'error at get_game_outcome: {str(e)}, failed to get game outcome\n'
-        helper_methods_logger.error(error_message)
-        raise custom_exceptions.GameOutcomeError(error_message) from e
+    return environ.board.outcome().result()
 ### end of get_game_outcome
 
 def get_game_termination_reason(environ) -> str:
-    """
-        returns a string that describes the reason for the game ending. It calls the `outcome` method 
-        on the chessboard.
-        Returns:
-            str: A string representing the reason for the game ending. 
-            If an error occurred while getting the termination reason, the returned string starts with 'error at 
-            get_game_termination_reason: ' and includes the error message.
-        Raises:
-            GameTerminationError: If the termination reason cannot be determined.
-    """
-    try:
-        return str(environ.board.outcome().termination)
-    except Exception as e:
-        error_message = f'error at get_game_termination_reason: {str(e)}, failed to get game end reason\n'
-        helper_methods_logger.error(error_message)
-        raise custom_exceptions.GameTerminationError(error_message) from e
+    return str(environ.board.outcome().termination)
 ### end of get_game_termination_reason
